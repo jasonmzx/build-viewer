d13ed71a460121660a0ad7230608424a635e0cc3
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2022-03-26 17:58:20 -0400

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import { Suspense } from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+
+import Box from "./components/Box";
+import AnimatedSphere from "./components/AnimatedSphere";
+import Iphone from "./components/Iphone";
+
+export default function App() {
+  return (
+    <Wrapper className="App">
+      <Background />
+      <TextSection />
+      <Canvas clasName="canvas">
+        <OrbitControls enableZoom={true} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+
+      <Canvas clasName="canvas">
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} />
+        <Suspense fallback={null}>
+          <AnimatedSphere />
+        </Suspense>
+      </Canvas>
+
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+`;
---
+++ frontend/src/components/Box.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
+import texture from "../images/map.jpg";
+
+export default function Box() {
+  const colorMap = useLoader(TextureLoader, texture);
+
+  return (
+    <mesh rotation={[90, 0, 20]}>
+      <boxBufferGeometry attach="geometry" args={[5, 5, 5]} />
+      <meshNormalMaterial attach="material" />
+      {/* <meshStandardMaterial map={colorMap} /> */}
+    </mesh>
+  );
+}
